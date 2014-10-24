3ba4bb863e6c81135ccf52d4fe433c467e1e4616
Matt Smith <runner_28@hotmail.com>
2014-10-23 22:33:38 -0400

    Initial formatting of the grid

--- client/app/grid/grid.controller.js
+++ client/app/grid/grid.controller.js
@@ -7,16 +7,4 @@ angular.module('buzzApp')
     $http.get('/api/words').success(function(words) {
       $scope.words = words;
     });
-
-    /*$scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-    $http.post('/api/words', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };*/
   });
